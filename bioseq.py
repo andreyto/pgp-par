faef912dac3330c947acc9d9c6220cd2bebea769
eventer <eventer@061605af-e407-0410-a945-9c8a22772c06>
2009-10-09 20:23:15 +0000

    Add a Fasta output class and a unittest for it.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/proteogenomics/trunk@29038 061605af-e407-0410-a945-9c8a22772c06

--- bioseq.py
+++ bioseq.py
@@ -10,32 +10,53 @@ class Sequence():
     A generic sequence class
     '''
 
-    def __init__(self,id=-1,seq='',desc=''):
+    def __init__(self,acc=-1,seq='',desc=''):
         '''
-        id is a sequence id
+        acc is a sequence id
         seq is the sequence string itself
         desc is a description of the sequence ie defline
         '''
-        self.id = id  
+        self.acc = acc  
         self.seq = seq
         self.desc = desc
         
-class FastaReader():
+class FlatFileReader():
     '''
-    A class that reads fasta files and produces Sequence objects.
-    Should be iterable as well.
-    '''
+    A parent class for all the classes that read flat files to
+    inherit from. Children should always be iterable.
+    '''        
+    def __init__(self,infile):
+        if type(infile) is file:
+            self.io = infile
 
-    def __init__(self,file):
-        if type(file) is file:
-            self.io = file
-
-        elif type(file) is str:
+        elif type(infile) is str:
             self.name = str
-            self.io = open( file, 'r')
+            self.io = open( infile, 'r')
         else:
             raise TypeError
 
+class FlatFileWriter():
+    '''
+    A parent class for all the classes that write flat files to
+    inherit from.
+    '''        
+    def __init__(self,infile):
+        if type(infile) is file:
+            self.io = infile
+
+        elif type(infile) is str:
+            self.name = str
+            self.io = open( infile, 'w')
+        else:
+            raise TypeError
+        
+    def close(self):
+        self.io.close()
+
+class FastaReader(FlatFileReader):
+    '''
+    A class that iterates through fasta files and produces Sequence objects.
+    '''
     def __iter__(self):
         seq = None
         seqList = None
@@ -48,14 +69,32 @@ class FastaReader():
                     yield seq
 
                 space = line.index(' ')
-                seq = Sequence( id=line[1:space] )
+                seq = Sequence( line[1:space] )
                 seq.desc = line[space+1:]
                 seqList = []
             else:
                 seqList.append(line)
-
+        # Don't forget to yield the final seq in the file
         if seqList:
-           seq.seq = ''.join(seqList)
-           yield seq
+            seq.seq = ''.join(seqList)
+            yield seq
 
         raise StopIteration
+    
+class FastaOut(FlatFileWriter):
+    '''
+    A class that takes Sequence objects and writes them out in fasta format 
+    to the given file. Includes a linesize accessor.
+    '''
+    def __init__(self,out):
+        FlatFileWriter.__init__(self, out)
+        self.linesize = 60
+        
+    def write(self,seq):
+        self.io.write(">%s %s\n" % (seq.acc,seq.desc))
+        i = 0
+        l = len(seq.seq)
+        while i < l:
+            self.io.write("%s\n" % seq.seq[i:i+self.linesize])
+            i += self.linesize
+            
--- unittests/testDBSetup.py
+++ unittests/testDBSetup.py
@@ -16,22 +16,35 @@ import bioseq
 
 class Test(unittest.TestCase):
 
+    IN = 'NC_004837.fa'
+    OUT = 'test.fa'
 
     def test6Frame(self):
         translate = SixFrameFasta.AbacusClass()
-        translate.ParseCommandLine(["-r","NC_004837.fa","-w","test1.fa","-c","Plasmid1"])
+        translate.ParseCommandLine(["-r",Test.IN,"-w",Test.OUT,"-c","Plasmid1"])
         translate.Main()       
-        self.assert_(filecmp.cmp("test1.fa","NC_004837.6frame.fa"))
-        os.remove("test1.fa")
+        self.assert_(filecmp.cmp(Test.OUT,"NC_004837.6frame.fa"))
+        os.remove(Test.OUT)
         os.remove("Temp.RT.fasta")
 
     def testFastaReader(self):
-        reader = bioseq.FastaReader('NC_004837.fa')
+        reader = bioseq.FastaReader(Test.IN)
         for seq in reader:
-            self.assertEqual('gi|31795327|ref|NC_004837.1|',seq.id)
+            self.assertEqual('gi|31795327|ref|NC_004837.1|',seq.acc)
             self.assertEqual('TGTAACGAACGGTG',seq.seq[0:14])
-            self.assertEqual('TACCCCGACCCCTG',seq.seq[-14::])
+            self.assertEqual('TACCCCGACCCCTG',seq.seq[-14:])
             self.assertEqual('Yersinia pestis KIM plasmid pPCP1, complete sequence',seq.desc)
+
+    def testFastaOut(self):
+        reader = bioseq.FastaReader(Test.IN)
+        output = bioseq.FastaOut(Test.OUT)
+        output.linesize = 70
+        for seq in reader:
+            output.write(seq)
+            
+        output.close()
+        self.assert_(filecmp.cmp(Test.OUT,Test.IN))
+        os.remove(Test.OUT)
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.test6Frame']
