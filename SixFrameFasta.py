8a4df3274601061be86c691fbd0aa111662c9abc
eventer <eventer@061605af-e407-0410-a945-9c8a22772c06>
2009-10-13 18:00:20 +0000

    Some more refactoring off duplicate code.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/proteogenomics/trunk@29103 061605af-e407-0410-a945-9c8a22772c06

--- SixFrameFasta.py
+++ SixFrameFasta.py
@@ -211,9 +211,6 @@ class AbacusClass(ProteinTranslationClass):
         with when we translate back in frame 1
         """    
         Handle = open(FileName, "rb")
-        Buffer = ""
-        ## Using 1 based stuff!, because people talk about frames 1,2,3 and not the zero based
-        StartInFrame = 1
         MaxLength = 200# number of dna letters in the buffer before we send out
         #set here before we loop through any dna.  Don't reset while looping.  then you would kill all genes overlapping a buffer
         self.FrameStrings = ['','','']
@@ -222,29 +219,8 @@ class AbacusClass(ProteinTranslationClass):
         
         self.TotalDNAIndex += 3
         
-        for Line in Handle.xreadlines():
-            if not Line.strip():
-                continue
-            Line = Line.strip()
-            #print ":%s:"%Line
-            if Line[0] == ">": 
-                # fasta header
-                continue
-            if len(Buffer) < MaxLength:           #add to string
-                Buffer += Line
-            else:          #it's big enough.  Process it, strip it back down, then add the line
-                LastTranslatedFrame = self.TranslateDNAOnReverse(Buffer, StartInFrame)
-                Buffer = Buffer[-2:] # get only the last two, because we translated the all up till that
-                #now add the line onto the buffer 
-                Buffer += Line
-                """ yes I know that I subtracted one in the return and now I'm about to add one
-                but this is the way that we can keep the terminology straight and know what
-                we are talking about.
-                """
-                StartInFrame = LastTranslatedFrame + 1  
-                            
-        #now at the end of the file, we send in the last buffer
-        LastTranslatedFrame = self.TranslateDNAOnReverse(Buffer, StartInFrame)
+        self.translateStrand( Handle, '-', MaxLength )
+
         # now after the last buffer's been processed, if there are some framebuffers that have yet to be written out, let's do that too
         for i in 0,1,2:
             if len(self.FrameStrings[i]) >= self.MinProteinLength:
@@ -269,44 +245,38 @@ class AbacusClass(ProteinTranslationClass):
             if AminoAcid == "*": # the terminator
                 WriteToFile = 1
                 
+            frame = CurrentFrame % 3
+            if frame == 0: # Frame 3 is last, an even multiple of 3
+                frame = 3
+
             #print Index, self.TotalDNAIndes, Codon, AminoAcid, (CurrentFrame % 3) 
-            if CurrentFrame %3 == 1:
-                #translate in frame 1
-                #print Index, self.TotalDNAIndex, Codon, AminoAcid, (CurrentFrame % 3)
-                if WriteToFile:
-                    self.writeFrame( 1, '-' )
-                    WriteToFile = 0 #I'm done writing, reset
-                else:                     
-                    #simply add on
-                    self.FrameStrings[0] += AminoAcid
-            elif CurrentFrame %3 == 2:
-                #translate in frame 2
-                if WriteToFile:
-                    self.writeFrame( 2, '-' )
-                    WriteToFile = 0 #I'm done writing, reset
-                else:                     
-                    #simply add on
-                    self.FrameStrings[1] += AminoAcid
-            elif CurrentFrame %3 == 0:
-                #translate in frame 3
-                if WriteToFile:
-                    self.writeFrame( 3, '-' )
-                    WriteToFile = 0 #I'm done writing, reset
-                else:                     
-                    #simply add on
-                    self.FrameStrings[2] += AminoAcid
+            if WriteToFile:
+                self.writeFrame( frame, '-' )
+                WriteToFile = 0 #I'm done writing, reset
+            else:                     
+                #simply add on
+                self.FrameStrings[frame-1] += AminoAcid
+
             CurrentFrame += 1
         #now we want to return the last translated frame.  Since we increment at the end of the loop
         # we need to subtract one to get back to the frame that last worked
         return (CurrentFrame - 1) % 3
         
-    def TranslateChromosome(self, FileName ):    
-        Handle = open(FileName, "rb")
-        Buffer = ""
+    def callTranslate( self, buffer, startInFrame, strand):
+        lastTranslatedFrame = None
+        if strand == '+':
+            lastTranslatedFrame = self.TranslateDNA(buffer, startInFrame)
+        elif strand == '-':
+            lastTranslatedFrame = self.TranslateDNAOnReverse(buffer, startInFrame)
+        else:
+            raise ValueError
+        return lastTranslatedFrame
+
+    def translateStrand( self, handle, strand, maxLength ):
+        Buffer = ''
         ## Using 1 based stuff!, because people talk about frames 1,2,3 and not the zero based
-        StartInFrame = 1
-        MaxLength = 10000# number of dna letters in the buffer before we send out
-        for Line in Handle.xreadlines():
+        startInFrame = 1
+        for Line in handle.xreadlines():
             if not Line.strip():
                 continue
             Line = Line.strip()
@@ -314,10 +284,11 @@ class AbacusClass(ProteinTranslationClass):
             if Line[0] == ">": 
                 # fasta header
                 continue
-            if len(Buffer) < MaxLength:           #add to string
+            if len(Buffer) < maxLength:           #add to string
                 Buffer += Line
             else:          #it's big enough.  Process it, strip it back down, then add the line
-                LastTranslatedFrame = self.TranslateDNA(Buffer, StartInFrame)
+                LastTranslatedFrame = self.callTranslate( Buffer, startInFrame, strand )
+
                 Buffer = Buffer[-2:] # get only the last two, because we translated the all up till that
                 #now add the line onto the buffer 
                 Buffer += Line
@@ -325,10 +296,17 @@ class AbacusClass(ProteinTranslationClass):
                 but this is the way that we can keep the terminology straight and know what
                 we are talking about.
                 """
-                StartInFrame = LastTranslatedFrame + 1  
-                            
+                startInFrame = LastTranslatedFrame + 1  
+
         #now at the end of the file, we send in the last buffer
-        LastTranslatedFrame = self.TranslateDNA(Buffer, StartInFrame)
+        self.callTranslate( Buffer, startInFrame, strand )
+
+    def TranslateChromosome(self, FileName ):    
+        Handle = open(FileName, "rb")
+        MaxLength = 10000# number of dna letters in the buffer before we send out
+                            
+        self.translateStrand( Handle, '+', MaxLength )
+
         #after this last buffer's been sent, output any latent ORFs
         for i in 0,1,2:
             if len(self.FrameStrings[i]) >= self.MinProteinLength:
@@ -353,31 +331,19 @@ class AbacusClass(ProteinTranslationClass):
                 WriteToFile = 1
                 
             #print Index, self.TotalDNAIndes, Codon, AminoAcid, (CurrentFrame % 3) 
-            if CurrentFrame %3 == 1:
-                #translate in frame 1
-                #print Index, self.TotalDNAIndex, Codon, AminoAcid, (CurrentFrame % 3)
-                if WriteToFile:
-                    self.writeFrame(1,'+')
-                    WriteToFile = 0 #I'm done writing, reset
-                else:                     
-                    #simply add on
-                    self.FrameStrings[0] += AminoAcid
-            elif CurrentFrame %3 == 2:
-                #translate in frame 2
-                if WriteToFile:
-                    self.writeFrame(2,'+')
-                    WriteToFile = 0 #I'm done writing, reset
-                else:                     
-                    #simply add on
-                    self.FrameStrings[1] += AminoAcid
-            elif CurrentFrame %3 == 0:
-                #translate in frame 3
-                if WriteToFile:
-                    self.writeFrame(3,'+')
-                    WriteToFile = 0 #I'm done writing, reset
-                else:                     
-                    #simply add on
-                    self.FrameStrings[2] += AminoAcid
+
+            frame = CurrentFrame % 3
+            if frame == 0: # Frame 3 is last, an even multiple of 3
+                frame = 3
+
+            #print Index, self.TotalDNAIndex, Codon, AminoAcid, (CurrentFrame % 3)
+            if WriteToFile:
+                self.writeFrame(frame,'+')
+                WriteToFile = 0 #I'm done writing, reset
+            else:                     
+                #simply add on
+                self.FrameStrings[frame-1] += AminoAcid
+
             CurrentFrame += 1
         #now we want to return the last translated frame.  Since we increment at the end of the loop
         # we need to subtract one to get back to the frame that last worked
