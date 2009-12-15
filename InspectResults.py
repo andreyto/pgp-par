1d7c2454c750f77c3f678fb9f31c0ffb7cb7ce8a
eventer <eventer@061605af-e407-0410-a945-9c8a22772c06>
2009-12-15 20:10:56 +0000

    Create a generator class for parsing inspect results, including a unit test.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/proteogenomics/trunk@30583 061605af-e407-0410-a945-9c8a22772c06

---
+++ InspectResults.py
@@ -0,0 +1,77 @@
+"""
+Constants and methods for parsing (Inspect) search results
+"""
+import os
+import bz2
+import random
+
+class Columns:
+    "Constants for which columns contain which data"
+    SpectrumFile = 0
+    ScanNumber = 1
+    Annotation = 2
+    ProteinName = 3
+    Charge = 4
+    MQScore = 5
+    Length = 6
+    NTT = 12
+    PValue = 13
+    FScore = 14
+    DeltaScoreAny = 15
+    DeltaScore = 16
+    ProteinID = 17
+    DBPos = 18
+    FileOffset = 19
+    LFDR = 20
+    
+class Parser():
+    def __init__(self, FilePath, wantedCols=Columns, MaxFilesToParse = None, QuietFlag = 0):
+        self.Columns = Columns
+        self.extensions = (".txt", ".filtered", ".res", ".csv", ".out")
+        self.wantedColumns = wantedCols
+        self.maxFiles = MaxFilesToParse
+        self.quiet = QuietFlag
+        self.filePath = FilePath
+        self.FileCount = 0
+
+    def __iter__(self):
+        """
+        Function for applying a Callback function to one search-results file, or to every
+        search-results file in a directory.
+        """
+        print "ResultsParser:%s" % self.filePath
+        if os.path.isdir(self.filePath):
+            FileNames = os.listdir(self.filePath)
+            random.shuffle(FileNames)
+        else:
+            FileNames = [self.filePath]
+
+        for FileNameIndex in range(len(FileNames)):
+            FileName = FileNames[FileNameIndex]
+            if not self.quiet:
+                print "(%s/%s) %s"%(FileNameIndex, len(FileNames), FileName)
+
+            fileHandle = None
+            (Stub, Extension) = os.path.splitext(FileName)
+            if Extension.lower() in self.extensions:
+                fileHandle = open( FileName )
+            elif Extension.lower() == '.bz2':
+                (prefix, Extension) = os.path.splitext(Stub)
+                if Extension.lower() in self.extensions:
+                    fileHandle = bz2.BZ2File( FileName )
+            else:
+                continue
+
+            self.FileCount += 1
+
+            for line in fileHandle:
+                cols = line.split("\t")
+                wanted = []
+                for i in range(len(cols)):
+                    if i in self.wantedColumns:
+                        wanted.append( cols[i] )
+                yield wanted
+
+            # Don't parse every single file, that will take too long!
+            if self.maxFiles != None and self.FileCount > self.maxFiles:
+                raise StopIteration 
---
+++ unittests/inspectResultsParserTest.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+'''
+Created on Oct 8, 2009
+
+@author: eli
+
+Some unit tests for the InspectResults Parser class.
+'''
+import unittest
+
+import InspectResults
+
+class Test(unittest.TestCase):
+
+    IN = 'inspect.txt.bz2'
+    ANNO = ['Annotation', 
+    'R.GGAAPA.P',
+    'G.GGAAAP.G',
+    'H.GGALAG.I',
+    'A.GGAIAG.A',
+    'Q.GGALGA.G',
+    'Q.GGAIGA.N',
+    'L.GGIAAG.A',
+    'G.GGLAAG.E',
+    'S.GGLAGA.R']
+    PVAL = ['p-value',
+    0.99972,
+    0.99990,
+    0.99999,
+    0.99999,
+    0.99999,
+    0.99999,
+    0.99999,
+    0.99999,
+    0.99999]
+
+
+    def testResultParser(self):
+        parser = InspectResults.Parser(self.IN, wantedCols=[
+                 InspectResults.Columns.Annotation,
+                 InspectResults.Columns.PValue])
+        i = 0
+        self.assertEqual( 10, len(self.ANNO) )
+        self.assertEqual( 10, len(self.PVAL) )
+
+        for vals in parser:
+            self.assertEqual( self.ANNO[i], vals[0] )
+            if i == 0:
+                self.assertEqual( self.PVAL[i], vals[1] )
+            else:
+                self.assertEqual( self.PVAL[i], float(vals[1]) )
+
+            i += 1
+
+        self.assertEqual( 10, i)
+
+if __name__ == "__main__":
+    unittest.main()
