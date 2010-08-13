6bb787bbed6a254b5af1a08e7ff6c6e56e8f4f18
eventer <eventer@061605af-e407-0410-a945-9c8a22772c06>
2010-08-13 13:20:41 +0000

    Forgot to scale the pca.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/proteogenomics/trunk@33010 061605af-e407-0410-a945-9c8a22772c06

--- codonBiplot.r
+++ codonBiplot.r
@@ -10,7 +10,7 @@ um = read.csv(unchangedMatrix, sep="\t", header=F)
 nm = read.csv(novelMatrix, sep="\t", header=F)
 
 pdf("CodonUsage.pca.pdf",width=8.5,height=11)
-upca = prcomp(um)
+upca = prcomp(um,scale=TRUE)
 biplot(upca,var.axes=F,xlabs=rep('*',nrow(um)),ylabs=rep('.',ncol(um)), col=c('black','white'))
 p = predict(upca,nm)
 points(p[,1],p[,2],col='blue',pch=16)
