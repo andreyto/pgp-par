7e0ca8967629e8710f99ec0db5d54fc7c1cc80b7
eventer <eventer@061605af-e407-0410-a945-9c8a22772c06>
2010-08-20 16:06:43 +0000

    Some improvements so locus numbers aren't duplicated.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/proteogenomics/trunk@33149 061605af-e407-0410-a945-9c8a22772c06

--- pickLocusPrefix.sh
+++ pickLocusPrefix.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-# get the most frequent prefix on the locus_tag, and round the count up to the next 1000
-for i in NC_*.original.tbl
+# Reverse sort the tbl's so largest is 1st, so we get the highest locus number 1st
+origInName=`\ls -s NC_[0-9]*.original.tbl 2>/dev/null | sort -nr | awk '{print $2}'`
+noOrig=`\ls -s NC_[0-9]*[0-9].tbl 2>/dev/null | sort -nr | awk '{print $2}'`
+
+# Keep track of previous, in case the locus prefix is shared with the plasmids
+prev='prev'
+prevNum=1000
+for i in $origInName $noOrig
 do
+  if [ ! -s $i ]
+  then
+      continue
+  fi
+# get the most frequent prefix on the locus_tag, and round the count up to the next 1000
   num_prefix=(`grep locus_tag $i | awk '{print $2}' |\
    perl -lne 'print $1 if /^(\D+)/' |\
     sort | uniq -c | sort -k1n,1 | tail -n 1 |\
@@ -10,7 +21,19 @@ do
   nc=${i%%.*} # strip everything after 1st .
   num=${num_prefix[0]}
   prefix=${num_prefix[1]}
+  # seen this locus before, use a higher number to avoid duplicates
+  if [ $prefix = $prev ]
+  then
+      num=$(( prevNum + 1000 ))
+  fi
   gff=$nc*.starts.gff
+  if [ ! -s $gff ]
+  then
+      echo No File $gff
+      continue
+  fi
+  prev=$prefix
+  prevNum=$num
   rejectFile=$nc.reject.list
   reject=''
   if [ -s $rejectFile ]
@@ -19,7 +42,7 @@ do
   fi
   setTbl="setTblStartsFromGFF.py"
   logFile="$nc.tbl.log"
-  echo $setTbl -g $gff -t $i -l $prefix -s $num $reject -o $nc.new.tbl
+  echo -g $gff -t $i -l $prefix -s $num $reject -o $nc.new.tbl
   # &> $logFile
   if [ -s $logFile ]
   then
