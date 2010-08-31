1c5b64e64389e62fb4f4b9d89c0b06101fae280e
eventer <eventer@061605af-e407-0410-a945-9c8a22772c06>
2010-08-31 18:12:00 +0000

    Update unittest for name change apply -> filterORF.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/proteogenomics/trunk@33211 061605af-e407-0410-a945-9c8a22772c06

--- unittests/pgFilterTest.py
+++ unittests/pgFilterTest.py
@@ -17,7 +17,7 @@ import GenomicLocations
 import PGPeptide
 
 class Test(unittest.TestCase):
-    
+
     def setUp(self):
         """This is some generic code that I don't feel like writing over again for each test
         """
@@ -45,16 +45,16 @@ class Test(unittest.TestCase):
         """Name: testSequenceComplexity_exclusivelyGA
         Description: This method is meant to test whether we remove peptides
         from ORF objects is they are exclusively GA in content. That's it.
-        
+
         NOTE: currently set up with the old GenomicLocations objects
         and not the new ones that Eli is writing. but we need tests now.
         """
         Filter = PGORFFilters.SequenceComplexityFilter()
-                
+
         #now make the encapsulating ORF
         ORF = PGPeptide.OpenReadingFrame(self.fastaLine, self.orfSeq)
         ORF.addLocatedPeptides([self.P1, self.P2, self.P3, self.P4])
-        
+
         #now run the filter
         ORF.filterPeptides( Filter.lowComplexFilter )
         #after running this filter, the one thing we care about for this unittest
@@ -65,7 +65,7 @@ class Test(unittest.TestCase):
         AcceptablePeptides = ["AGGAGQWEIUPIUFGBVA", "AGGAGGQHGGGAG"]
         for Peptide in ORF.peptideIter():
             self.assert_(Peptide.aminos in AcceptablePeptides)
-            
+
     def testMinPeptide(self):
         """Name: testMinPeptide
         Description: this is just to test that our code surrounding the 
@@ -85,13 +85,13 @@ class Test(unittest.TestCase):
         ORF1.addLocatedPeptides( ListOf1 )
         ORF2.addLocatedPeptides( ListOf2 )
         ORF5.addLocatedPeptides( ListOf5 )
-        
+
         #now we run the tests
-        self.assertEqual(FilterRequire1.apply(ORF0), 1) # 1 says DELETEME NOW
-        self.assertEqual(FilterRequire1.apply(ORF1), 0) 
-        self.assertEqual(FilterRequire5.apply(ORF2), 1) 
-        self.assertEqual(FilterRequire2.apply(ORF2), 0) 
-        self.assertEqual(FilterRequire5.apply(ORF5), 0) 
+        self.assertEqual(FilterRequire1.filterORF(ORF0), 1) # 1 says DELETEME NOW
+        self.assertEqual(FilterRequire1.filterORF(ORF1), 0) 
+        self.assertEqual(FilterRequire5.filterORF(ORF2), 1) 
+        self.assertEqual(FilterRequire2.filterORF(ORF2), 0) 
+        self.assertEqual(FilterRequire5.filterORF(ORF5), 0) 
 
         # Also test the FilterList
         filters = PGORFFilters.FilterList([FilterRequire5], "dummyfile.delme")
@@ -105,7 +105,7 @@ class Test(unittest.TestCase):
         in that it should require a unique peptide.
         """
         Filter = PGORFFilters.UniquenessFilter()
-        
+
         ListOf5 = [self.P1, self.P2, self.P3, self.P4, self.P5] #brackets make a list Sam, parens make a tuple
         #now this is tricky, but important, if I don't declare something as unique, I assume that it is not
         #so only P1 and P3 are unique.
@@ -114,9 +114,9 @@ class Test(unittest.TestCase):
         ORFHasUnique.addLocatedPeptides( ListOf5 )
         ORFLacksUnique = PGPeptide.OpenReadingFrame(self.fastaLine, self.orfSeq)
         ORFLacksUnique.addLocatedPeptides( ListOfBad )
-        
-        self.assertEqual(Filter.apply(ORFHasUnique), 0)
-        self.assertEqual(Filter.apply(ORFLacksUnique), 1)
+
+        self.assertEqual(Filter.filterORF(ORFHasUnique), 0)
+        self.assertEqual(Filter.filterORF(ORFLacksUnique), 1)
 
     def testTrypticFilter(self):
         "Name: test the tryptic filters of open reading frames"
@@ -131,11 +131,11 @@ class Test(unittest.TestCase):
         ORFLacksTryptic.addLocatedPeptides( ListLacksTryptic )
         ORFUnset = PGPeptide.OpenReadingFrame(self.fastaLine, self.orfSeq)
         ORFUnset.addLocatedPeptides( ListUnset )
-       
-        self.assertEqual(Filter.apply(ORFHasTryptic), 0)
-        self.assertEqual(Filter.apply(ORFLacksTryptic), 1)
-        self.assertEqual(Filter.apply(ORFUnset), 1)
-        
+
+        self.assertEqual(Filter.filterORF(ORFHasTryptic), 0)
+        self.assertEqual(Filter.filterORF(ORFLacksTryptic), 1)
+        self.assertEqual(Filter.filterORF(ORFUnset), 1)
+
 
 
 if __name__ == "__main__":
