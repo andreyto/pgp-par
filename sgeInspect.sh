85142213081f10cc7785deffa745c46d6e6b5232
eventer <eventer@061605af-e407-0410-a945-9c8a22772c06>
2010-09-29 14:58:33 +0000

    Initial try to do both pepnovo and pvalue and msgf.
    git-svn-id: http://svn.jcvi.org/ANNOTATION/proteogenomics/trunk@33404 061605af-e407-0410-a945-9c8a22772c06

--- sgeInspect.sh
+++ sgeInspect.sh
@@ -14,8 +14,11 @@ rundir=$PWD
 results=$rundir/ResultsX
 filter=$results/filter
 inspectOut=$results/$SGE_TASK_ID.txt
-rescoreOut=${inspectOut/.txt/.msgf}
-filterOut=${rescoreOut/ResultsX/ResultsX/filter}
+pepnovoOut=${inspectOut/.txt/.res}
+pvalueIn=$results/$SGE_TASK_ID.txt
+pvalueOut=${inspectOut/.txt/.pval}
+msgfOut=${inspectOut/.txt/.msgf}
+filterOut=${msgfOut/ResultsX/ResultsX/filter}
 
 PepNovoDir=/usr/local/depot/projects/PGP/productionPepNovo
 MSGF=/usr/local/depot/projects/PGP/MSGF/MSGF.jar
@@ -30,20 +33,35 @@ then
     exit 1
 fi
 
-if $java -jar $MSGF -inspect $inspectOut -d mzxml -x 0 > $rescoreOut
+cd $PepNovoDir
+ulimit -c 0 # PepNovo cores have filled up the disk before
+
+if ./PepNovo_bin -model CID_IT_TRYP -PTMs 'C+57' -file $mzxml -rescore_inspect $inspectOut $pepnovoOut
+then
+    echo "PepNovo success"
+    pvalueIn=$pepnovoOut
+else
+    echo "PepNovo failure"
+    pepnovoOut=''
+fi
+
+# PValue the inspect or pepnovo results
+$exe_path/PValue.py -r $pvalueIn -w $pvalueOut -p 0.1 -S 0.5 -1 -H &> pvalue.log 
+
+if $java -jar $MSGF -inspect $pvalueOut -d mzxml -x 0 > $msgfOut
 then
     echo "MSGF success"
     if [ ! -d $filter ]
     then
         mkdir $filter
     fi
-    $exe_path/FilterInspectMSGF.py -p 1e-6 -r $rescoreOut -o $filterOut
+    $exe_path/FilterInspectMSGF.py -p 1e-6 -r $msgfOut -o $filterOut
 else
     echo "MSGF failure"
     rescoreOut=''
     filterOut=''
 fi
     
-bzip2 $inspectOut $rescoreOut $filterOut
+bzip2 $inspectOut $pepnovoOut $pvalueOut $msgfOut $filterOut
 
 touch $rundir/Done/$SGE_TASK_ID
