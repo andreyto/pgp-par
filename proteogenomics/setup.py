d0046807731adbcdef7aed966078c1ae6456f2df
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-10-14 01:21:39 -0400

    Refactoring. Extension in a separate dir. Biopython without numpy.

--- proteogenomics/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-indexSearchExt = Extension('_IndexSearch',
-     sources = ['IndexSearch.cpp'])
-
-setup (name = 'IndexSearch',
-    version = '1.0',
-    description = 'IndexSearch extension build',
-    ext_modules = [indexSearchExt])
