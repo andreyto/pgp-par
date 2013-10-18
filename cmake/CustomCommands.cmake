978b62fc61c82943c07067444d48103b84a26bd4
Andrey Tovchigrechko <atovtchi@jcvi.org>
2013-10-18 11:06:59 -0400

    Using argument parsing for CMake functions and doing assertions

--- cmake/CustomCommands.cmake
+++ cmake/CustomCommands.cmake
@@ -1,8 +1,26 @@
-## Create cutsom_command that also sets environment variables
-function(make_script_command SCRIPT_NAME SCRIPT_BODY COMMAND SCRIPT_FILE)
-    set(scr_name ${CMAKE_CURRENT_BINARY_DIR}/${SCRIPT_NAME}.cmake)
-    file(WRITE ${scr_name} "${SCRIPT_BODY}")
+include(Checks)
+include(CMakeParseArguments)
+
+## Write CMake script_body to a file and return command line
+## for running the script and the file name of the script
+## These values can be used as arguments of custom_command():
+## add_custom_command(COMMAND script_command DEPENDS script_file ...).
+## If your script body is a multiline string that contains:
+## "
+## set(ENV{VAR}=${val})
+## execute_process(COMMAND whatever ...)
+## "
+## and 'val' is defined in your CMakeLists.txt,
+## then this is the way to generate custom_command target that 
+## is built under a modified shell environment.
+function(make_script_command) 
+    set(oneValueArgs SCRIPT_NAME SCRIPT_BODY VAR_SCRIPT_COMMAND VAR_SCRIPT_FILE)
+    cmake_parse_arguments(arg "" "${oneValueArgs}" "" ${ARGN})
+    at_assert_defined_prefixed(arg ${oneValueArgs})
+    set(scr_name ${CMAKE_CURRENT_BINARY_DIR}/${script_name}.cmake)
+    file(WRITE ${scr_name} "${script_body}")
     set(cmd ${CMAKE_COMMAND} -P ${scr_name})
-    set (${COMMAND} ${cmd} PARENT_SCOPE)
+    set (${script_command} ${cmd} PARENT_SCOPE)
+    set (${script_file} ${scr_name} PARENT_SCOPE)
 endfunction()
 
