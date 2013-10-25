7cffe65f4d0524bb7b77d8d680ca5ab6955edbad
Andrey Tovchigrechko <andreyto@gmail.com>
2013-10-25 05:24:31 +0000

    Commented out sample batch options for test execution

--- config/htc/toolchain.cmake
+++ config/htc/toolchain.cmake
@@ -2,6 +2,6 @@
 ##like below to some subprocess unchanged. The quotations and STRING variable
 ##type below do work (with extra shlex splitting done in the test Python code).
 ##Follow this example when editing the value, or you will break the test.
-set(PGP_TEST_PGP_RUN_EXTRA_OPTIONS "-T sge -B '-P 0534 -l fast -b n -S /bin/bash'" CACHE STRING "Pass these optional arguments to pgp_run when testing. Tune this to your cluster configuration." FORCE)
+#set(PGP_TEST_PGP_RUN_EXTRA_OPTIONS "-T sge -B '-P 0534 -l fast -b n -S /bin/bash'" CACHE STRING "Pass these optional arguments to pgp_run when testing. Tune this to your cluster configuration." FORCE)
 
 
